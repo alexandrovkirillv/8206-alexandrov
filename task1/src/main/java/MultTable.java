71d722616ca23b1d89f0cefc84cf96ea44d9747c
Alexandrov Kirill <alexandrovkirillv@mail.ru>
2019-11-05 01:11:16 +0700

    task1

--- task1/src/main/java/MultTable.java
+++ task1/src/main/java/MultTable.java
@@ -48,19 +48,38 @@ public class MultTable {
 
     private static void printTable(int[][] multTableArray, int sizeArray) {
 
+        String tableFormatter= "";
+        String lineSplitter = "";
+
+        if (sizeArray > 0 & sizeArray < 4) {
+            tableFormatter = "%2s";
+            lineSplitter = "_+";
+
+        } else if (sizeArray >= 4 & sizeArray  < 10) {
+            tableFormatter = "%3s";
+            lineSplitter = "__+";
+        } else if (sizeArray >= 10 & sizeArray < 32) {
+            tableFormatter = "%4s";
+            lineSplitter = "___+";
+        } else if (sizeArray == 32) {
+            tableFormatter = "%5s";
+            lineSplitter = "____+";
+        }
+
+
         for (int i = 0; i < sizeArray; i++) {
 
             System.out.println("");
             for (int j = 0; j < sizeArray; j++) {
 
-                System.out.printf("%5s", multTableArray[i][j] + "|");
+                System.out.printf(tableFormatter, multTableArray[i][j] + "|");
 
             }
 
             System.out.println("");
             for (int j = 0; j < sizeArray; j++) {
 
-                System.out.printf("%5s", "____+");
+                System.out.printf(tableFormatter, lineSplitter);
             }
 
         }
