c05e7e80ea06d83211549160524b9bf63c780827
Alexandrov Kirill <alexandrovkirillv@mail.ru>
2019-11-23 12:27:05 +0700

    task2 try3

--- task2/src/main/java/ru/focusstart/tomsk/Main.java
+++ task2/src/main/java/ru/focusstart/tomsk/Main.java
@@ -14,21 +14,20 @@ public class Main {
 
     private static String outPutFileName = "";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
         List<String> linesFromFile = readFile(args);
         Figure figure = createFigure(parsingLines(linesFromFile), linesFromFile);
-        try {
-            writeData(figure);
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Wrong parameters");
-        }
-
+        writeData(figure);
     }
 
     private static List<String> readFile(String[] args) {
-
         List<String> linesFromFile = new ArrayList<>();
+
+        if (args.length < 1) {
+            System.out.println("No arguments");
+            throw new ArrayIndexOutOfBoundsException();
+        }
 
         try (LineIterator lineIterator = new LineIterator(new FileReader(args[0]))) {
             while (lineIterator.hasNext()) {
@@ -37,9 +36,8 @@ public class Main {
         } catch (Exception e) {
             System.out.println("File not found");
         }
-        try {
+        if (args.length > 1) {
             outPutFileName = args[1];
-        } catch (ArrayIndexOutOfBoundsException ignored) {
         }
 
         return linesFromFile;
@@ -47,62 +45,58 @@ public class Main {
 
 
     private static int[] parsingLines(List<String> linesFromFile) {
+        if (linesFromFile.isEmpty() | linesFromFile.size() < 2) {
+            System.out.println("File is empty or no parameters");
+            throw new NullPointerException();
+        }
 
-        int[] parameters = new int[0];
+        int[] parameters = new int[linesFromFile.size() - 1];
 
         try {
-            parameters = new int[linesFromFile.size() - 1];
-
             for (int i = 1; i < linesFromFile.size(); i++) {
                 parameters[i - 1] = Integer.parseInt(linesFromFile.get(i));
             }
-
         } catch (Exception e) {
             System.out.println("Data is incorrect");
         }
+
+        if (linesFromFile.get(0).equals("RECTANGLE") & parameters.length < 2) {
+            System.out.println("Only one parameter for Rectangle");
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
         return parameters;
     }
 
-    private static Figure createFigure(int[] parameters, List<String> linesFromFile) {
-        Figure figure = null;
+    private static Figure createFigure(int[] parameters, List<String> linesFromFile) throws Exception {
+        Figure figure;
 
-        try {
-            if (linesFromFile.get(0).equals("CIRCLE")) {
-                figure = new Circle(parameters);
-                figure.getName();
-            } else if (linesFromFile.get(0).equals("SQUARE")) {
-                figure = new Square(parameters);
-                figure.getName();
-            } else if (linesFromFile.get(0).equals("RECTANGLE")) {
-                figure = new Rectangle(parameters);
-                figure.getName();
-            } else {
-                System.out.println("Figure is not CIRCLE, SQUARE, RECTANGLE");
-            }
-        } catch (Exception e) {
-
+        if (linesFromFile.get(0).equals("CIRCLE")) {
+            figure = new Circle(parameters);
+        } else if (linesFromFile.get(0).equals("SQUARE")) {
+            figure = new Square(parameters);
+        } else if (linesFromFile.get(0).equals("RECTANGLE")) {
+            figure = new Rectangle(parameters);
+        } else {
+            System.out.println("Figure is not CIRCLE, SQUARE, RECTANGLE");
+            throw new Exception();
         }
 
         return figure;
     }
 
     private static void writeData(Figure figure) throws IOException {
-
-        try {
-            if (outPutFileName.equals("")) {
+        if (outPutFileName.equals("")) {
+            for (String s : figure.getDescription()) {
+                System.out.println(s);
+            }
+        } else {
+            try (Writer printWriter = new PrintWriter(new File(outPutFileName))) {
                 for (String s : figure.getDescription()) {
                     System.out.println(s);
-                }
-            } else {
-                try (Writer printWriter = new PrintWriter(new File(outPutFileName))) {
-                    for (String s : figure.getDescription()) {
-                        System.out.println(s);
-                        printWriter.write(s + "\n");
-                    }
+                    printWriter.write(s + "\n");
                 }
             }
-        } catch (NullPointerException e) {
-
         }
     }
 }
