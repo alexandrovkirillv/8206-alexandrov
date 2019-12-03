74907f40527e2f373f288f7d581d7c6bdb89a8b7
kirill <alexandrovkirillv@mail.ru>
2019-12-03 16:17:38 +0700

    task3 try 4

--- task3/src/main/java/ru/focusstart/tomsk/Main.java
+++ task3/src/main/java/ru/focusstart/tomsk/Main.java
@@ -2,18 +2,20 @@ package ru.focusstart.tomsk;
 
 import org.apache.log4j.Logger;
 
+import java.util.Deque;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class Main {
 
     private static final int N = 5;
     private static final int M = 5;
-    private static final int tN = 100;
-    private static final int tM = 100;
+    private static final int tN = 2000;
+    private static final int tM = 2000;
     private static final int S = 5;
 
     public static void main(String[] args) {
-        Store store = new Store();
-        Producer producer = new Producer(store);
-        Consumer consumer = new Consumer(store);
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
 
         for (int i = 0; i < N; i++) {
             new Thread(producer, "Producer-" + i).start();
@@ -25,42 +27,45 @@ public class Main {
     }
 
     static class Store {
-        private int product = 0;
+        private static Deque<String> deque = new LinkedBlockingDeque<>(S);
+        int counter = 0;
         Logger logger = Logger.getLogger("");
 
-        synchronized void get() throws InterruptedException {
-            while (product < 1) {
-                try {
-                    wait();
-                } catch (InterruptedException ignored) {
-                }
+        void put() throws InterruptedException {
+            if (deque.size() < S) {
+                String product = createProduct();
+                logger.info(product + " produced ");
+                deque.offerFirst(product);
+                logger.info("add " + product);
+                Thread.sleep(tN);
+            } else {
+                logger.info("no more space");
+                Thread.sleep(tN);
             }
-            Thread.sleep(tM);
-            product--;
-            logger.info("delete product #" + product);
-            notifyAll();
         }
 
-        synchronized void put() throws InterruptedException {
-            while (product >= S) {
-                try {
-                    wait();
-                } catch (InterruptedException ignored) {
-                }
+        void get() throws InterruptedException {
+            if (!deque.isEmpty()) {
+                String product = deque.pollLast();
+                logger.info("delete " + product);
+                logger.info(product + " is used");
+                Thread.sleep(tM);
+            } else {
+                logger.info("no product");
+                Thread.sleep(tM);
             }
-            Thread.sleep(tN);
-            product++;
-            logger.info("add product #" + product);
-            notifyAll();
+        }
+
+        public String createProduct() {
+            String product;
+            counter++;
+            product = "product" + counter;
+            return product;
         }
     }
 
     static class Producer implements Runnable {
-        Store store;
-
-        Producer(Store store) {
-            this.store = store;
-        }
+        Store store = new Store();
 
         public void run() {
             while (true) {
@@ -74,11 +79,7 @@ public class Main {
     }
 
     static class Consumer implements Runnable {
-        Store store;
-
-        Consumer(Store store) {
-            this.store = store;
-        }
+        Store store = new Store();
 
         public void run() {
             while (true) {
